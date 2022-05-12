25f00e842b133ac9ea8693fcf686608476ede12e
viper-timon <viper-timon@mail.ru>
2022-05-12 22:56:55 +0300

    Tasks 14 16 done: done q : git yes no clear

---
+++ Task_16/Program.cs
@@ -0,0 +1,15 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите 1ое число -> ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите 2ое число -> ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 * num1 == num2) Console.WriteLine("Второе число явялется квадратом первого");
+else if (num2 * num2 == num1) Console.WriteLine("Первое число явялется квадратом второго");
+else Console.WriteLine("Не являются квадратами друг друга");
--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -6,18 +6,22 @@
 // 12-> 2
 // 85 -> 8
 
+int FindMax (int num)
+{
+    int num1 = num / 10;
+    int num2 = num % 10;
+    int max = num1;
+    if (num1 < num2) max = num2;
+    return max;
+}
+
 int number = new Random().Next(10, 100);
 Console.WriteLine ($"Number = {number}");
-int maxNum = 
+int maxNum = FindMax (number);
+Console.WriteLine ($"Max = {maxNum}");
+
 // int firstDigit = number/10;
 // int secondDigit = number%10;
 // Console.WriteLine ($"1st = {firstDigit}");
 // Console.WriteLine ($"2nd = {secondDigit}");
 // Console.WriteLine (firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
-
-int FindMaxNumber 
-{
-    int firstDigit = num / 10;
-    int secondDigit = num % 10;
-    return firstDigit > secondDigit ? firstDigit : secondDigit;
-}
