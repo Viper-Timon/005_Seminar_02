a471cac604fbde2616b61ac4c5b49a2772c68ba5
viper-timon <viper-timon@mail.ru>
2022-05-12 21:46:49 +0300

    Tasks 11 12 done

--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -8,9 +8,16 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine ($"Number = {number}");
-int firstDigit = number/10;
-int secondDigit = number%10;
-Console.WriteLine ($"1st = {firstDigit}");
-Console.WriteLine ($"2nd = {secondDigit}");
+int maxNum = 
+// int firstDigit = number/10;
+// int secondDigit = number%10;
+// Console.WriteLine ($"1st = {firstDigit}");
+// Console.WriteLine ($"2nd = {secondDigit}");
+// Console.WriteLine (firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
 
-Console.WriteLine (firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
+int FindMaxNumber 
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
